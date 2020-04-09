8d25915e586d69ed54b86f3cf24415263b0fb61e
tathithuyvn89 <61693986+tathithuyvn89@users.noreply.github.com>
2020-04-09 16:37:09 +0700

    thay doi code

--- TH_tinhtienlai.java
+++ TH_tinhtienlai.java
@@ -16,9 +16,9 @@ public class TH_tinhtienlai {
         System.out.println("Enter annual interest rate in percentage : ");
         interestRate= scanner.nextDouble();
         double totalInterest=0;
-        for ( int i=0; i<month;i++){ // vong lap for k co y nghia
+//         for ( int i=0; i<month;i++){ // vong lap for k co y nghia
             totalInterest=money*(interestRate/100)/12*month;
-        }
+//         }
         System.out.println("Total interest : " + totalInterest);
     }
 }
